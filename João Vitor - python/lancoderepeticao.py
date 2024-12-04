0d870974a50581dd0239296b979b0e51fb033fb1
João vitor <147009431+joaovitor10000@users.noreply.github.com>
2024-12-04 18:43:07 -0300

    Add files via upload

---
+++ João Vitor - python/lancoderepeticao.py
@@ -0,0 +1,18 @@
+#Outra atividade 03/07/24
+#x=0
+
+#while x < 5:
+    #nome = input('Qual o seu nome? ')
+    #x=x+1
+
+x=0
+animal =[]
+
+while x < 5:
+    animais =input ('5 Animais que começe com a letra do seu nome? ')
+    animal.append(animais)
+    x=x+1
+
+    print(animal)
+
+
---
+++ joao-10-10/cadastro.php
@@ -0,0 +1,9 @@
+
+<?php
+$server = "localhost";
+$user = "root";
+$password ="";
+$dbname ="bdlojas2022";
+
+$conn = mysqli_connect($server,$user,$password,$dbname);
+?>
